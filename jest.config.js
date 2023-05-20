cb6ba3fc3e77e7b5e27d9c6e15a66760492e9e13
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-20 00:59:16 -0600

    refactor(jest-config): Changes `Jest Config` file to TS.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-import {pathsToModuleNameMapper} from 'ts-jest';
-import {compilerOptions} from './tsconfig.json';
-
-/** @type {import('jest').Config} */
-module.exports = {
-    preset: 'ts-jest',
-    moduleDirectories: ['node_modules', '<rootDir>'],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
-};
