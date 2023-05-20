7ea0b46d85d71c0d6338da117b69f0218f40bcdd
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-19 23:58:28 -0600

    style(ide): Adds `sort imports by module` code style setting.

--- src/functions/getActiveUsers.ts
+++ src/functions/getActiveUsers.ts
@@ -1,5 +1,5 @@
-import { UserType }    from '@/types';
 import { USER_STATUS } from '@/constants';
+import { UserType }    from '@/types';
 
 
 export const getActiveUsers = (users: UserType[]) => users.filter(({ status }) => status === USER_STATUS.active);
