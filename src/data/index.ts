0a3f056189617ba6bde39f029757c58c7f5444b9
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-20 01:00:27 -0600

    refactor(data): Changes plain `status` string value to constant implementation.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,32 +1,33 @@
-import { UserType } from '@/types';
+import { USER_STATUS } from '@/constants/index';
+import { UserType }    from '@/types';
 
 
 const rocky: UserType = {
     name:          'Rocky',
     favoriteFood:  'Sushi',
     favoriteMovie: 'Back to The Future',
-    status:        'Inactive'
+    status:        USER_STATUS.inactive
 };
 
 const miroslav: UserType = {
     name:          'Miroslav',
     favoriteFood:  'Sushi',
     favoriteMovie: 'American Psycho',
-    status:        'Inactive'
+    status:        USER_STATUS.inactive
 };
 
 const donny: UserType = {
     name:          'Donny',
     favoriteFood:  'Singapore chow mei fun',
     favoriteMovie: 'The Princess Bride',
-    status:        'Inactive'
+    status:        USER_STATUS.inactive
 };
 
 const matt: UserType = {
     name:          'Matt',
     favoriteFood:  'Brisket Tacos',
     favoriteMovie: 'The Princess Bride',
-    status:        'Active'
+    status: USER_STATUS.active
 };
 
 export const usersCollection = [
